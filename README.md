173c727d445ac96cc976e224241b3d11e1fef610
georginaelena <georgina.elena@ui.ac.id>
2024-05-08 17:03:55 +0700

    Experiment 3.2: Be Creative

--- README.md
+++ README.md
@@ -8,3 +8,12 @@ Kelas : AdvProg - B
 Berikut adalah tampilan Yew Webchat setelah saya run dan coba mengirimkan pesan untuk 2 user pada window localhost:8000 yang berbeda:
 
 ![](https://i.imgur.com/NB60D9F.png)
+
+## 3.2 Be Creative!
+
+Saya mengubah tema warna menjadi pink dan juga menambah kan avatar.
+![](https://i.imgur.com/kGP9XwG.png)
+
+![](https://i.imgur.com/T8htcrm.png)
+
+![](https://i.imgur.com/qh7GlIr.png)
--- src/components/chat.rs
+++ src/components/chat.rs
@@ -93,8 +93,7 @@ impl Component for Chat {
                             .map(|u| UserProfile {
                                 name: u.into(),
                                 avatar: format!(
-                                    "https://avatars.dicebear.com/api/adventurer-neutral/{}.svg",
-                                    u
+                                    "https://i.pinimg.com/564x/d8/9c/52/d89c522f09bdc53a90d9363b182ad275.jpg"
                                 )
                                     .into(),
                             })
@@ -140,7 +139,7 @@ impl Component for Chat {
         let submit = ctx.link().callback(|_| Msg::SubmitMessage);
         html! {
              <div class="flex w-screen">
-                 <div class="flex-none w-56 h-screen bg-gray-100">
+                 <div class="flex-none w-56 h-screen bg-pink-200">
                      <div class="text-xl p-3">{"Users"}</div>
                      {
                          self.users.clone().iter().map(|u| {
@@ -163,8 +162,8 @@ impl Component for Chat {
                      }
                  </div>
                  <div class="grow h-screen flex flex-col">
-                     <div class="w-full h-14 border-b-2 border-gray-300"><div class="text-xl p-3">{"💬 Chat!"}</div></div>
-                     <div class="w-full grow overflow-auto border-b-2 border-gray-300">
+                    <div class="w-full h-14 border-b-2 border-gray-300 bg-pink-100"><div class="text-xl p-3">{"🩷 Chat!"}</div></div>
+                    <div class="w-full grow overflow-auto border-b-2 border-gray-300 bg-pink-100">
                          {
                              self.messages.iter().map(|m| {
                                  let user = self.users.iter().find(|u| u.name == m.from).unwrap();
@@ -189,9 +188,9 @@ impl Component for Chat {
                          }
 
                      </div>
-                     <div class="w-full h-14 flex px-3 items-center">
+                     <div class="w-full h-14 flex px-3 items-center bg-pink-100">
                          <input ref={self.chat_input.clone()} type="text" placeholder="Message" class="block w-full py-2 pl-4 mx-3 bg-gray-100 rounded-full outline-none focus:text-gray-700" name="message" required=true />
-                         <button onclick={submit} class="p-3 shadow-sm bg-blue-600 w-10 h-10 rounded-full flex justify-center items-center color-white">
+                         <button onclick={submit} class="p-3 shadow-sm bg-pink-500 w-10 h-10 rounded-full flex justify-center items-center color-white">
                              <svg fill="#000000" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="fill-white">
                                  <path d="M0 0h24v24H0z" fill="none"></path><path d="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z"></path>
                              </svg>
